4f377b4cb60f371e112d6b6506de911390a70b44
José Escrich <jescrich@gmail.com>
2017-08-02 00:03:03 -0300

    Update server.js to handle / and disable resend code

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
 	res.sendfile('index.html', { root: path.join(__dirname, '..', 'build') });
 });
 
