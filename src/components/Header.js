a1b79d63387f44026fa1f97990721b138eb9fd46
José Escrich <jescrich@gmail.com>
2017-08-18 14:15:39 -0300

    Fix header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,19 +21,29 @@ class Header extends Component {
 				<div className="container-fluid bkg-001">
 					<div className="container">
 						{isLoggedIn && (
-							<div className="row visible-sm visible-md visible-lg">
-								<div className="col-sm-3 pad-12">
-									<Link to="/home">
-										<img src="images/logoNMPch.png" className="img-responsive" alt="" />
-									</Link>
-								</div>
+							<div>
+								<div className="row visible-sm visible-md visible-lg">
+									<div className="col-sm-3 pad-12">
+										<Link to="/home">
+											<img src="images/logoNMPch.png" className="img-responsive" alt="" />
+										</Link>
+									</div>
 
-								<div className='col-sm-3 pad-12'>
-									<div className="spacer-24"></div>
-									<p className="text-right"><img src="images/logo-tel.png" alt="" /></p>
+									<div className='col-sm-3 pad-12'>
+										<div className="spacer-24"></div>
+										<p className="text-right"><img src="images/logo-tel.png" alt="" /></p>
+									</div>
+									<div className="col-sm-6 pad-12">
+										<UserInfo />
+									</div>
 								</div>
-								<div className="col-sm-6 pad-12">
-									<UserInfo />
+								<div className="row visible-xs">
+									<div className="col-xs-6 pad-12">
+										<img src="images/logoNMPch.png" className="img-responsive" alt="" />
+									</div>
+									<div className="col-xs-6 pad-12">
+										<UserInfo />
+									</div>
 								</div>
 							</div>
 						)}
@@ -52,17 +62,11 @@ class Header extends Component {
 							</div>
 						)}
 					</div>
-					<div className="row visible-xs">
-						<div className="col-xs-6 pad-12">
-							<img src="images/logoNMPch.png" className="img-responsive" alt="" />
-						</div>
-						<div className="col-xs-6 pad-12">
-							<UserInfo />
-						</div>
-					</div>
-					<MMNav />
-					<div className="spacer-24"></div>
+
+					
 				</div>
+				<MMNav />
+				<div className="spacer-24"></div>
 			</div>
 
 		);
