a1eec9595e006baa5dc83d43324b4c2d1dcb4cc6
Ricardo <thrafiker@gmail.com>
2018-02-20 10:12:55 -0600

    advances

--- server/routes/DowloadPdf.js
+++ server/routes/DowloadPdf.js
@@ -45,7 +45,11 @@ module.exports = (req, res) => {
 
 			}
 
+
 			let fileBase64String = b1.archivoBase64;
+			if(fileBase64String===undefined)
+				res.send("No existe documento");
+
 			let nameFile = b1.numeroFolio ? "mimonte_folio_"+b1.numeroFolio : "mimonte";
 			/**Procesamiento de dato para descarga PDF */
 			
